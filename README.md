83d3fe66c8c0d6a4708ee828e57a68df5494801c
Dave Page <dpage@pgadmin.org>
2023-12-18 12:59:05 +0000

    Convert multiple images at once, removing alpha.

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ Random code for Pinorami's Cosmic Unicorn display
 
 ## convert_image.py
 
-This is an extremely quick hack of code to convert a PNG file (or other 
-formats if the code is tweaked) into a 32x32 image, expressed in JSON as a 
-grid of RGB values. This allows us to easily render whatever parts of the 
-image we want, or to render it pixel by pixel or whatever, to give a nice
-effect.
+This is an extremely quick hack of code to convert a directory of images 
+into a 32x32 image, expressed in JSON as a grid of RGB values. This allows 
+us to easily render whatever parts of the image we want, or to render it 
+pixel by pixel or whatever, to give a nice effect.
 
 ### TODO
 
--- convert_image.py
+++ convert_image.py
@@ -1,17 +1,50 @@
 from PIL import Image
 import numpy as np
 import json
+import os
 
-image_name = 'pgconfeu'
+FOLDER = 'christmas/images'
 
-raw = Image.open(f'{image_name}.png', mode='r').convert('RGB', colors=8)
 
-raw.thumbnail((32, 32))
+# From: https://stackoverflow.com/a/35859141
+def remove_transparency(im, bg_colour=(255, 255, 255)):
 
-image = np.array(raw)
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
 
-image = image.tolist()
-json.dump(image, open(f'{image_name}.json', 'w'),
-          separators=(',', ':'),
-          sort_keys=True,
-          indent=0)
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').split()[-1]
+
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+
+    else:
+        return im
+
+
+def convert_image(filename):
+    raw = Image.open(os.path.join(FOLDER, filename), mode='r')
+    raw = remove_transparency(raw, (0, 0, 0))
+    raw = raw.convert('RGB', colors=8)
+
+    image = np.array(raw)
+
+    image = image.tolist()
+
+    json.dump(image, open(os.path.join(FOLDER, filename[:-4] + '.json'), 'w'),
+              separators=(',', ':'),
+              sort_keys=True,
+              indent=0)
+
+
+for image in os.listdir(FOLDER):
+    # check if the image ends with png
+    if image.endswith('.png') or image.endswith('.jpg'):
+        print(f'Converting {image}...')
+        convert_image(image)
+
+
